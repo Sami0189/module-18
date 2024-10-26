4eb2357b08fd518b3ba4303f8a095ce811d8536e
Sami0189 <samisaimon28@gmail.com>
2024-10-26 20:24:42 +0600

    forloop

---
+++ forloop.js
@@ -0,0 +1,16 @@
+// const numbers = [23, 42, 53,64,612,745,34];
+
+// console.log(numbers);
+// for(const num of numbers){
+//     console.log(num);
+// }
+
+
+
+
+const fruits = ['apple','orange','banana','waterlemon','coconut'];
+
+for(const fruit of fruits){
+      console.log(fruit);
+      console.log('i want to eat');
+}
---
+++ while.js
@@ -0,0 +1,14 @@
+// const number = 9;
+
+// if(number<10){
+//     console.log('choto ekta number paisi');
+// }
+//     console.log('something new is cooking');
+
+let num =1;
+while (num<5){
+    console.log('looooping');
+    // num = num + 1;
+    //    num +=1
+    num++;
+}
