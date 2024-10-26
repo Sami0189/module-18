e7e63000f62c5fb39092d6cbfabb1a11a8a6c3ba
Sami0189 <samisaimon28@gmail.com>
2024-10-26 20:48:09 +0600

    important

---
+++ range.js
@@ -0,0 +1,11 @@
+// let num = 1;
+// while(num<=1000){
+//     console.log('who are you');
+//     num = num + 1;
+// }
+
+let num = 50 ;
+while(num<=100){
+    console.log(num);
+    num = num + 1;
+}
---
+++ sum.js
@@ -0,0 +1,8 @@
+let num = 1;
+let sum = 0;
+while(num<=10){
+    console.log(num);
+    console.log('sum',sum);
+    sum = sum + num;
+    num = num + 1;
+}
