0a2c52cf66a0bc20ebb3b407a6fb328084ef60a8
Sami0189 <samisaimon28@gmail.com>
2024-10-27 14:32:11 +0600

    multipiclation

--- do-while.js
+++ do-while.js
@@ -4,9 +4,34 @@
 //     i++
 // }
 
-let n =10;
-do{
-    console.log(n);
-    n++;
+// let n =10;
+// do{
+//     console.log(n);
+//     n++;
+// }
+// while(n<5)
+
+
+// let a = 5;
+// let b = 10;
+// let result = a*b;
+// console.log(result);
+
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log(`5 x ${i} = ${5 * i}`);
+  
+// }
+
+// for(let i = 0;i<5;i=i+3){
+//     console.log(i);
+// }
+
+
+const array =[1,2,3,4,5,6,7];
+for (let i=1 ; i<array.length;i=i+1){
+    if(i==5){
+        continue;
+    }
+    console.log(array[i]);
 }
-while(n<5)
