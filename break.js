a58c62e07537fbd03f2d904ca351fe7d4adb6923
Sami0189 <samisaimon28@gmail.com>
2024-10-31 22:03:34 +0600

    finish

--- break.js
+++ break.js
@@ -22,12 +22,12 @@
 //  }
 
 
-// for(let i =1; i<10;i++){
-//     if(i%2===1){
-//         continue;
-//     }
-//     console.log(i);
-// }
+for(let i =1; i<10;i++){
+    if(i%2===1){
+        continue;
+    }
+    console.log(i);
+}
 
 
 let n = 0;
